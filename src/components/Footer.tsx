b920054e3dc9ade5d9ab0688493187eb868381f4
Junya <junjia.liu@pm.me>
2025-12-14 15:08:31 +0000

    update footer, get rid of no worky script

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,7 @@ export function Footer() {
 	return (
 		<footer className="text-center py-8 text-ctp-subtext0 text-sm">
 			<p className="mt-1">
-				<a href="https://github.com/junyali/cuties-site" className="text-ctp-mauve hover:underline">
-					see the code here!!
-				</a>
+				made by junya <span className="text-ctp-pink">{'<3'}</span> <span>(see the code <a href="https://github.com/junyali/cuties-site" className="text-ctp-mauve hover:underline">here!!</a>)</span>
 			</p>
 		</footer>
 	);
